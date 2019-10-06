e019e01f8585f1466d75b125facf583120d71a1d
Joe <joedravarol@gmail.com>
2019-10-06 23:37:41 +0100

    Add Gameboard module

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ const Player = (name, mark) => {
   
   return { getName, getMark };
 };
+
+const Gameboard = ( () => {
+  // Private Variable
+  let gameboard = [ "", "", "", "", "", "", "", "", "" ];
+  
+  // Public Methods
+  const get = () => gameboard;
+  
+  const update = (index, mark) => {
+    if ( !isDataFilled(index) ) {
+      gameboard[index] = mark;
+    };
+  };
+
+  const reset = () => {
+    for (let i in gameboard) {
+      gameboard[i] = "";
+    };
+  };
+
+  const isDataFilled = dataIndex => {
+    return gameboard[dataIndex] ? true : false;
+  };
+
+  return { get, update, reset, isDataFilled };
+})();
